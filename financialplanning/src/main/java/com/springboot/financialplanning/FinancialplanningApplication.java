d178a2d0670b576d2c5f2d9213fa1863d6b5cfc2
Bitra pavan venkatesh <105623115+pavanvenkatesh2277@users.noreply.github.com>
2023-11-10 18:17:27 +0530

    intial setup

---
+++ financialplanning/src/main/java/com/springboot/financialplanning/FinancialplanningApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.financialplanning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialplanningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialplanningApplication.class, args);
+	}
+
+}
---
+++ financialplanning/src/test/java/com/springboot/financialplanning/FinancialplanningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.financialplanning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialplanningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
